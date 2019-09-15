3b233d831eb8c141b4a92a9e27a8b3f7da6dfa0a
Hénoc HOUNSA <henochounsa@gmail.com>
2019-09-15 09:00:00 +0100

    fix: add phone masking serializer to logger

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -1,5 +1,12 @@
 import pino from 'pino';
 
+function maskPhone(phone: string): string {
+  if (!phone || phone.length < 4) return '****';
+  // Show only last 4 digits for privacy
+  const last4 = phone.slice(-4);
+  return `+***${last4}`;
+}
+
 const logger = pino({
   level: process.env.LOG_LEVEL || 'info',
   prettyPrint: process.env.NODE_ENV !== 'production'
@@ -8,6 +15,10 @@ const logger = pino({
   base: {
     service: 'routing-engine',
     pid: process.pid,
+  },
+  serializers: {
+    phone: maskPhone,
+    err: pino.stdSerializers.err,
   },
   timestamp: pino.stdTimeFunctions.isoTime,
 });
