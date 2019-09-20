81f5b0e2d85f2bd040660e97ee68a8d2185692a1
Hénoc HOUNSA <henochounsa@gmail.com>
2019-09-20 10:15:00 +0100

    fix: stricter joi schema, better error formatting

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,63 +3,81 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const schema = Joi.object({
+const envSchema = Joi.object({
   NODE_ENV: Joi.string()
     .valid('development', 'production', 'test')
     .default('development'),
-  PORT: Joi.number().default(3000),
+  PORT: Joi.number().port().default(3000),
   LOG_LEVEL: Joi.string()
     .valid('fatal', 'error', 'warn', 'info', 'debug', 'trace')
     .default('info'),
 
+  // Database
   DATABASE_URL: Joi.string().required(),
+
+  // Redis
   REDIS_URL: Joi.string().default('redis://localhost:6379'),
 
+  // API security
   API_KEY_HEADER: Joi.string().default('X-API-Key'),
   MERCHANT_API_KEYS: Joi.string().required(),
+
+  // Webhook
   WEBHOOK_BASE_URL: Joi.string().uri().required(),
 
+  // Kkiapay
   KKIAPAY_PUBLIC_KEY: Joi.string().required(),
   KKIAPAY_PRIVATE_KEY: Joi.string().required(),
   KKIAPAY_SECRET_KEY: Joi.string().required(),
   KKIAPAY_BASE_URL: Joi.string().uri().default('https://api.kkiapay.me'),
-  KKIAPAY_SANDBOX: Joi.boolean().default(false),
+  KKIAPAY_SANDBOX: Joi.boolean().truthy('true').falsy('false').default(false),
 
+  // FedaPay
   FEDAPAY_SECRET_KEY: Joi.string().required(),
   FEDAPAY_BASE_URL: Joi.string().uri().default('https://api.fedapay.com/v1'),
-  FEDAPAY_SANDBOX: Joi.boolean().default(false),
+  FEDAPAY_SANDBOX: Joi.boolean().truthy('true').falsy('false').default(false),
 
+  // CinetPay
   CINETPAY_API_KEY: Joi.string().required(),
   CINETPAY_SITE_ID: Joi.string().required(),
   CINETPAY_BASE_URL: Joi.string().uri().default('https://api-checkout.cinetpay.com/v2'),
 
+  // FeexPay
   FEEXPAY_API_KEY: Joi.string().required(),
   FEEXPAY_BASE_URL: Joi.string().uri().default('https://api.feexpay.me'),
 
+  // PayDunya
   PAYDUNYA_MASTER_KEY: Joi.string().required(),
   PAYDUNYA_PRIVATE_KEY: Joi.string().required(),
   PAYDUNYA_TOKEN: Joi.string().required(),
   PAYDUNYA_BASE_URL: Joi.string().uri().default('https://app.paydunya.com/api/v1'),
 
+  // Routing
   MAX_ATTEMPTS_PER_TRANSACTION: Joi.number().integer().min(1).max(5).default(3),
   SCORING_WINDOW_HOURS: Joi.number().integer().min(1).max(24).default(1),
   SCORING_MIN_SAMPLES: Joi.number().integer().min(1).default(10),
-  DEFAULT_CURRENCY: Joi.string().default('XOF'),
+  DEFAULT_CURRENCY: Joi.string().uppercase().length(3).default('XOF'),
 
-  STATUS_POLL_INTERVAL_MS: Joi.number().integer().default(5000),
-  STATUS_POLL_MAX_ATTEMPTS: Joi.number().integer().default(12),
-  WEBHOOK_RETRY_ATTEMPTS: Joi.number().integer().default(5),
-  WEBHOOK_RETRY_BACKOFF_MS: Joi.number().integer().default(1000),
+  // Jobs
+  STATUS_POLL_INTERVAL_MS: Joi.number().integer().min(1000).default(5000),
+  STATUS_POLL_MAX_ATTEMPTS: Joi.number().integer().min(1).default(12),
+  WEBHOOK_RETRY_ATTEMPTS: Joi.number().integer().min(1).max(10).default(5),
+  WEBHOOK_RETRY_BACKOFF_MS: Joi.number().integer().min(100).default(1000),
 });
 
-const { error, value } = schema.validate(process.env, { allowUnknown: true, abortEarly: false });
+const { error, value: validatedEnv } = envSchema.validate(process.env, {
+  allowUnknown: true,
+  abortEarly: false,
+});
 
 if (error) {
-  const details = error.details.map((d: { message: string }) => `  - ${d.message}`).join('\n');
-  throw new Error(`Config validation failed:\n${details}`);
+  const missing = error.details
+    .map((d: { message: string }) => `  • ${d.message}`)
+    .join('\n');
+  throw new Error(`\n[config] Startup aborted — env validation failed:\n${missing}\n`);
 }
 
-export const env = value as {
+export const env = validatedEnv as {
   NODE_ENV: string;
   PORT: number;
   LOG_LEVEL: string;
