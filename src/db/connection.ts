53efd1d7602ec9987258ff9363634b2959d6087f
Hénoc HOUNSA <henochounsa@gmail.com>
2019-10-14 09:30:00 +0100

    add: TypeORM database connection module

---
+++ src/db/connection.ts
@@ -0,0 +1,43 @@
+import { createConnection, Connection } from 'typeorm';
+import { env } from '../config/env';
+import logger from '../config/logger';
+
+let connection: Connection | null = null;
+
+export async function connectDatabase(): Promise<Connection> {
+  if (connection && connection.isConnected) {
+    return connection;
+  }
+
+  try {
+    connection = await createConnection({
+      type: 'postgres',
+      url: env.DATABASE_URL,
+      entities: [__dirname + '/entities/*.entity{.ts,.js}'],
+      migrations: [__dirname + '/migrations/*{.ts,.js}'],
+      synchronize: false,
+      logging: env.NODE_ENV === 'development' ? ['error', 'warn'] : false,
+    });
+
+    logger.info('Database connected');
+    return connection;
+  } catch (err) {
+    logger.error({ err }, 'Database connection failed');
+    throw err;
+  }
+}
+
+export async function disconnectDatabase(): Promise<void> {
+  if (connection && connection.isConnected) {
+    await connection.close();
+    connection = null;
+    logger.info('Database disconnected');
+  }
+}
+
+export function getConnection(): Connection {
+  if (!connection || !connection.isConnected) {
+    throw new Error('Database not connected. Call connectDatabase() first.');
+  }
+  return connection;
+}
