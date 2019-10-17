aaf95fe11e649dbacf0cc95dfe7d0f758aca8343
Hénoc HOUNSA <henochounsa@gmail.com>
2019-10-17 09:30:00 +0100

    add: entities barrel export

---
+++ src/db/entities/index.ts
@@ -0,0 +1,4 @@
+export { TransactionEntity, TxStatus } from './transaction.entity';
+export { AttemptEntity, AttemptStatus } from './attempt.entity';
+export { ProviderMetricEntity } from './providerMetric.entity';
+export { AuditLogEntity } from './auditLog.entity';
