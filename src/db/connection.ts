1bcd97eb0937bfe791985afe8362bd866732d92e
Hénoc HOUNSA <henochounsa@gmail.com>
2019-11-18 09:30:00 +0100

    fix: add retry logic to database connection on startup

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -5,27 +5,42 @@ import logger from '../config/logger';
 
 let connection: Connection | null = null;
 
-export async function connectDatabase(): Promise<Connection> {
+const MAX_RETRIES = 5;
+const RETRY_DELAY_MS = 3000;
+
+async function sleep(ms: number): Promise<void> {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+export async function connectDatabase(retries = MAX_RETRIES): Promise<Connection> {
   if (connection && connection.isConnected) {
     return connection;
   }
 
-  try {
-    connection = await createConnection({
-      type: 'postgres',
-      url: env.DATABASE_URL,
-      entities: [__dirname + '/entities/*.entity{.ts,.js}'],
-      migrations: [__dirname + '/migrations/*{.ts,.js}'],
-      synchronize: false,
-      logging: env.NODE_ENV === 'development' ? ['error', 'warn'] : false,
-    });
+  for (let attempt = 1; attempt <= retries; attempt++) {
+    try {
+      connection = await createConnection({
+        type: 'postgres',
+        url: env.DATABASE_URL,
+        entities: [__dirname + '/entities/*.entity{.ts,.js}'],
+        migrations: [__dirname + '/migrations/*{.ts,.js}'],
+        synchronize: false,
+        logging: env.NODE_ENV === 'development' ? ['error', 'warn'] : false,
+      });
 
-    logger.info({ event: 'db_connected' }, 'Database connected');
-    return connection;
-  } catch (err) {
-    logger.error({ err, event: 'db_connect_failed' }, 'Database connection failed');
-    throw err;
+      logger.info({ event: 'db_connected', attempt }, 'Database connected');
+      return connection;
+    } catch (err) {
+      if (attempt === retries) {
+        logger.error({ err, event: 'db_connect_failed' }, 'Database connection failed after all retries');
+        throw err;
+      }
+      logger.warn({ attempt, retries, retryIn: RETRY_DELAY_MS }, 'DB connection failed, retrying...');
+      await sleep(RETRY_DELAY_MS);
+    }
   }
+
+  throw new Error('Unreachable');
 }
 
 export async function disconnectDatabase(): Promise<void> {
