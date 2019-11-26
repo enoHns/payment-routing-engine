4bf6b8bb3e6e8e7554225db5484a7a3ad642f70a
Hénoc HOUNSA <henochounsa@gmail.com>
2019-11-26 14:00:00 +0100

    update: index.ts bootstrap connects db and redis

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,31 @@
+import 'reflect-metadata';
+import { connectDatabase, disconnectDatabase } from './db/connection';
+import { getRedisClient, closeRedis } from './cache/redis';
 import logger from './config/logger';
 
 async function bootstrap() {
   logger.info('Payment Routing Engine — starting up...');
-  // TODO: initialize database, redis, server, job workers
+
+  await connectDatabase();
+  getRedisClient(); // initialise connection
+
+  logger.info('Infrastructure ready. Routing engine online.');
+
+  // TODO: start Fastify server
+  // TODO: register BullMQ workers
 }
 
+async function gracefulShutdown(signal: string) {
+  logger.info({ signal }, 'Shutdown signal received');
+  await disconnectDatabase();
+  await closeRedis();
+  process.exit(0);
+}
+
+process.on('SIGTERM', () => gracefulShutdown('SIGTERM'));
+process.on('SIGINT', () => gracefulShutdown('SIGINT'));
+
 bootstrap().catch(err => {
-  console.error('Fatal startup error:', err);
+  logger.error({ err }, 'Fatal startup error');
   process.exit(1);
 });
