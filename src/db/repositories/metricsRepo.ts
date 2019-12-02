e7d06445e0a63cc7c9cc02363686167b5b1ce09d
Hénoc HOUNSA <henochounsa@gmail.com>
2019-12-02 09:30:00 +0100

    fix: explicit uuid in metrics upsert, clean up column quoting

--- src/db/repositories/metricsRepo.ts
+++ src/db/repositories/metricsRepo.ts
@@ -12,27 +12,27 @@ export async function upsertMetricWindow(
   latencyMs?: number,
 ): Promise<void> {
   const conn = getConnection();
+  const lat = latencyMs ?? 0;
 
-  // Use raw query for atomic increment (no lost-update race condition)
   if (outcome === 'SUCCESS') {
     await conn.query(
       `INSERT INTO provider_metrics
-         ("providerName", "operator", "country", "windowStart", "successCount", "totalLatencyMs", "sampleCount", "updatedAt")
-       VALUES ($1, $2, $3, $4, 1, $5, 1, now())
-       ON CONFLICT ("providerName", "operator", "country", "windowStart")
+         (id, "providerName", operator, country, "windowStart", "successCount", "totalLatencyMs", "sampleCount", "updatedAt")
+       VALUES (uuid_generate_v4(), $1, $2, $3, $4, 1, $5, 1, now())
+       ON CONFLICT ("providerName", operator, country, "windowStart")
        DO UPDATE SET
          "successCount"   = provider_metrics."successCount" + 1,
          "totalLatencyMs" = provider_metrics."totalLatencyMs" + $5,
          "sampleCount"    = provider_metrics."sampleCount" + 1,
          "updatedAt"      = now()`,
-      [providerName, operator, country, windowStart, latencyMs ?? 0],
+      [providerName, operator, country, windowStart, lat],
     );
   } else {
     await conn.query(
       `INSERT INTO provider_metrics
-         ("providerName", "operator", "country", "windowStart", "failureCount", "sampleCount", "updatedAt")
-       VALUES ($1, $2, $3, $4, 1, 1, now())
-       ON CONFLICT ("providerName", "operator", "country", "windowStart")
+         (id, "providerName", operator, country, "windowStart", "failureCount", "sampleCount", "updatedAt")
+       VALUES (uuid_generate_v4(), $1, $2, $3, $4, 1, 1, now())
+       ON CONFLICT ("providerName", operator, country, "windowStart")
        DO UPDATE SET
          "failureCount" = provider_metrics."failureCount" + 1,
          "sampleCount"  = provider_metrics."sampleCount" + 1,
@@ -60,16 +60,18 @@ export async function getRecentMetrics(
     .getMany();
 }
 
-export async function getAllProviderCombinations(): Promise<Array<{ providerName: string; operator: string; country: string }>> {
-  return getConnection()
+export async function getAllProviderCombinations(): Promise<
+  Array<{ providerName: string; operator: string; country: string }>
+> {
+  const rows = await getConnection()
     .getRepository(ProviderMetricEntity)
     .createQueryBuilder('m')
-    .select(['m.providerName', 'm.operator', 'm.country'])
-    .distinct(true)
-    .getRawMany()
-    .then(rows => rows.map(r => ({
-      providerName: r.m_providerName,
-      operator: r.m_operator,
-      country: r.m_country,
-    })));
+    .select('DISTINCT m.providerName, m.operator, m.country')
+    .getRawMany();
+
+  return rows.map(r => ({
+    providerName: r.providerName,
+    operator: r.operator,
+    country: r.country,
+  }));
 }
