9dd2cce4bdb18b19753d67e9f61707f4d8944487
Hénoc HOUNSA <henochounsa@gmail.com>
2020-02-03 09:00:00 +0100

    add: getEligibleProviders from providerConfig

---
+++ src/core/providerRegistry.ts
@@ -0,0 +1,29 @@
+import providerConfig from '../data/providerConfig.json';
+
+export interface ProviderConfig {
+  name: string;
+  displayName: string;
+  supportedCountries: string[];
+  supportedOperators: Record<string, string[]>;
+  priority: number;
+  active: boolean;
+}
+
+export function getEligibleProviders(country: string, operator: string): ProviderConfig[] {
+  return (providerConfig.providers as ProviderConfig[])
+    .filter(p => {
+      if (!p.active) return false;
+      if (!p.supportedCountries.includes(country)) return false;
+      const ops = p.supportedOperators[country];
+      return ops && ops.includes(operator);
+    })
+    .sort((a, b) => a.priority - b.priority);
+}
+
+export function getProviderByName(name: string): ProviderConfig | undefined {
+  return (providerConfig.providers as ProviderConfig[]).find(p => p.name === name);
+}
+
+export function getAllActiveProviders(): ProviderConfig[] {
+  return (providerConfig.providers as ProviderConfig[]).filter(p => p.active);
+}
