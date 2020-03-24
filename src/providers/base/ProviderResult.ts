028383fc16eb95ee266ca2c1962c6de60b23929c
Hénoc HOUNSA <henochounsa@gmail.com>
2020-03-24 10:00:00 +0100

    add: ProviderResult, error codes, isRetryable

---
+++ src/providers/base/ProviderResult.ts
@@ -0,0 +1,24 @@
+export interface ProviderError {
+  code: string;
+  message: string;
+  retryable: boolean;
+}
+
+export const PROVIDER_ERROR_CODES = {
+  TIMEOUT:           'PROVIDER_TIMEOUT',
+  NETWORK:           'PROVIDER_NETWORK_ERROR',
+  INVALID_ACCOUNT:   'PROVIDER_INVALID_ACCOUNT',
+  INSUFFICIENT_FUNDS:'PROVIDER_INSUFFICIENT_FUNDS',
+  LIMIT_EXCEEDED:    'PROVIDER_LIMIT_EXCEEDED',
+  DUPLICATE:         'PROVIDER_DUPLICATE_TRANSACTION',
+  UNKNOWN:           'PROVIDER_UNKNOWN_ERROR',
+} as const;
+
+export type ProviderErrorCode = typeof PROVIDER_ERROR_CODES[keyof typeof PROVIDER_ERROR_CODES];
+
+export function isRetryable(code: string): boolean {
+  return [
+    PROVIDER_ERROR_CODES.TIMEOUT,
+    PROVIDER_ERROR_CODES.NETWORK,
+  ].includes(code as ProviderErrorCode);
+}
