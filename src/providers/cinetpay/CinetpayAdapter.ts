265abf2dcbffaa8590c0a165c1d9f0ad9a94e083
Hénoc HOUNSA <henochounsa@gmail.com>
2020-04-02 09:00:00 +0100

    add: CinetPay adapter (IP-whitelist webhook)

---
+++ src/providers/cinetpay/CinetpayAdapter.ts
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import { ProviderAdapter, PaymentRequest, PaymentResponse } from '../base/ProviderAdapter';
+import { env } from '../../config/env';
+import logger from '../../config/logger';
+
+const BASE_URL = 'https://api-checkout.cinetpay.com/v2';
+const TIMEOUT  = 10000;
+
+export class CinetpayAdapter implements ProviderAdapter {
+  readonly name = 'cinetpay';
+
+  async initiatePayment(req: PaymentRequest): Promise<PaymentResponse> {
+    const response = await axios.post(
+      `${BASE_URL}/payment`,
+      {
+        apikey:                env.CINETPAY_API_KEY,
+        site_id:               env.CINETPAY_SITE_ID,
+        transaction_id:        req.transactionId,
+        amount:                req.amount,
+        currency:              req.currency,
+        description:           `Payment ${req.transactionId}`,
+        return_url:            req.webhookUrl,
+        notify_url:            req.webhookUrl,
+        customer_phone_number: req.phone,
+        channels:              'MOBILE_MONEY',
+        lang:                  'fr',
+      },
+      { timeout: TIMEOUT },
+    );
+
+    logger.debug({ transactionId: req.transactionId, provider: this.name }, 'Payment initiated');
+
+    return {
+      providerTxId: req.transactionId, // CinetPay uses our ID
+      status: 'PENDING',
+      rawResponse: response.data,
+    };
+  }
+
+  verifyWebhook(payload: unknown, _signature?: string): boolean {
+    // CinetPay uses IP whitelist, not HMAC — validate payload structure
+    if (typeof payload !== 'object' || payload === null) return false;
+    const p = payload as Record<string, unknown>;
+    return typeof p.cpm_trans_id === 'string' && typeof p.cpm_result === 'string';
+  }
+}
