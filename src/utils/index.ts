226091b1e4703a38e40d365f3b8f2fa6a8955d5b
Hénoc HOUNSA <henochounsa@gmail.com>
2020-04-30 10:00:00 +0100

    add: utils barrel export

---
+++ src/utils/index.ts
@@ -0,0 +1,4 @@
+export { normalizePhone, getCallingCode } from './phone';
+export { safeHmacCompare } from './crypto';
+export { normalizeProviderError } from './httpError';
+export type { NormalizedError } from './httpError';
