2496560dfa0ad533d6682c1efdaa629109f95b98
Hénoc HOUNSA <henochounsa@gmail.com>
2020-05-14 10:00:00 +0100

    refactor: attemptRepo now uses Prisma client

--- src/db/repositories/attemptRepo.ts
+++ src/db/repositories/attemptRepo.ts
@@ -1,39 +1,31 @@
-import { getConnection } from '../connection';
-import { AttemptEntity, AttemptStatus } from '../entities/attempt.entity';
+import { AttemptStatus, Attempt } from '@prisma/client';
+import { getPrismaClient } from '../prismaClient';
 
 export interface CreateAttemptData {
   transactionId: string;
-  providerName: string;
-  score: number;
+  providerName:  string;
+  score:         number;
 }
 
-export async function createAttempt(data: CreateAttemptData): Promise<AttemptEntity> {
-  const repo = getConnection().getRepository(AttemptEntity);
-  const attempt = repo.create(data);
-  return repo.save(attempt);
+export async function createAttempt(data: CreateAttemptData): Promise<Attempt> {
+  return getPrismaClient().attempt.create({ data });
 }
 
 export async function updateAttempt(
   id: string,
-  data: Partial<Pick<AttemptEntity, 'status' | 'providerTxId' | 'latencyMs' | 'errorCode' | 'errorMessage' | 'webhookPayload' | 'resolvedAt'>>,
+  data: Partial<Pick<Attempt, 'status' | 'providerTxId' | 'latencyMs' | 'errorCode' | 'errorMessage' | 'webhookPayload' | 'resolvedAt'>>,
 ): Promise<void> {
-  await getConnection().getRepository(AttemptEntity).update(id, data);
+  await getPrismaClient().attempt.update({ where: { id }, data });
 }
 
-export async function findAttemptByProviderTxId(providerTxId: string): Promise<AttemptEntity | undefined> {
-  return getConnection()
-    .getRepository(AttemptEntity)
-    .findOne({ where: { providerTxId } });
+export async function findAttemptByProviderTxId(providerTxId: string): Promise<Attempt | null> {
+  return getPrismaClient().attempt.findFirst({ where: { providerTxId } });
 }
 
-export async function findAttemptsByTransactionId(transactionId: string): Promise<AttemptEntity[]> {
-  return getConnection()
-    .getRepository(AttemptEntity)
-    .find({ where: { transactionId }, order: { createdAt: 'ASC' } });
+export async function findAttemptsByTransactionId(transactionId: string): Promise<Attempt[]> {
+  return getPrismaClient().attempt.findMany({ where: { transactionId }, orderBy: { createdAt: 'asc' } });
 }
 
 export async function countAttempts(transactionId: string): Promise<number> {
-  return getConnection()
-    .getRepository(AttemptEntity)
-    .count({ where: { transactionId } });
+  return getPrismaClient().attempt.count({ where: { transactionId } });
 }
