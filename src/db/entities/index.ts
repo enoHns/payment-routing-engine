e959573e401abfdf26149d0d6e52e40bd18058c0
Hénoc HOUNSA <henochounsa@gmail.com>
2020-05-19 09:00:00 +0100

    chore: remove TypeORM entities, migrations, ormconfig (Prisma only)

--- src/db/entities/index.ts
+++
@@ -1,4 +0,0 @@
-export { TransactionEntity, TxStatus } from './transaction.entity';
-export { AttemptEntity, AttemptStatus } from './attempt.entity';
-export { ProviderMetricEntity } from './providerMetric.entity';
-export { AuditLogEntity } from './auditLog.entity';
