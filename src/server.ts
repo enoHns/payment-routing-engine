b1586816d01a0fb42d83f6b7a974e9184db77fad
Hénoc HOUNSA <henochounsa@gmail.com>
2020-07-02 09:00:00 +0100

    fix: server.ts registers errorHandler and health routes

--- src/server.ts
+++ src/server.ts
@@ -2,36 +2,30 @@ import fastify, { FastifyInstance } from 'fastify';
 import helmet from 'fastify-helmet';
 import rateLimit from 'fastify-rate-limit';
 import { getRedisClient } from './cache/redis';
+import { errorHandlerPlugin } from './plugins/errorHandler';
+import { healthRoutes } from './routes/health';
 import logger from './config/logger';
 import { env } from './config/env';
 
 export async function buildServer(): Promise<FastifyInstance> {
-  const server = fastify({
-    logger: false, // We use pino directly
-    trustProxy: true,
-  });
+  const server = fastify({ logger: false, trustProxy: true });
 
-  // Security headers
   await server.register(helmet);
-
-  // Rate limiting — uses Redis store
   await server.register(rateLimit, {
-    max:     env.RATE_LIMIT_MAX,
-    timeWindow: env.RATE_LIMIT_WINDOW_MS,
-    redis:   getRedisClient(),
-    keyGenerator: (req) => req.headers['x-forwarded-for'] as string || req.ip,
+    max:          env.RATE_LIMIT_MAX,
+    timeWindow:   env.RATE_LIMIT_WINDOW_MS,
+    redis:        getRedisClient(),
+    keyGenerator: (req) => (req.headers['x-forwarded-for'] as string) || req.ip,
   });
 
-  // Request logging
+  await server.register(errorHandlerPlugin);
+  await server.register(healthRoutes);
+
   server.addHook('onRequest', async (req) => {
-    logger.info({ method: req.method, url: req.url, reqId: req.id }, 'Incoming request');
+    logger.info({ method: req.method, url: req.url, reqId: req.id }, '→');
   });
-
   server.addHook('onResponse', async (req, reply) => {
-    logger.info(
-      { method: req.method, url: req.url, statusCode: reply.statusCode, reqId: req.id },
-      'Request completed',
-    );
+    logger.info({ method: req.method, url: req.url, status: reply.statusCode, reqId: req.id }, '←');
   });
 
   return server;
