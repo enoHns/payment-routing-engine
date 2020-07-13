5af7e5bf9363744d386a527bcbe3314ead891e0a
Hénoc HOUNSA <henochounsa@gmail.com>
2020-07-13 10:00:00 +0100

    update: index.ts starts Fastify 3 server on bootstrap

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import { connectWithRetry, disconnectPrisma } from './db/prismaClient';
 import { getRedisClient, closeRedis } from './cache/redis';
+import { buildServer } from './server';
 import logger from './config/logger';
+import { env } from './config/env';
 
 async function bootstrap() {
   logger.info('Payment Routing Engine — starting up...');
+
   await connectWithRetry();
   getRedisClient();
-  logger.info('Infrastructure ready.');
-  // TODO: start Fastify server
+
+  const server = await buildServer();
+  await server.listen(env.PORT, '0.0.0.0');
+  logger.info({ port: env.PORT }, 'HTTP server listening');
+
   // TODO: register BullMQ workers
 }
 
