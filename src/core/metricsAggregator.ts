d2e955665d84c595ccfcfed052becfb73dc453ce
Hénoc HOUNSA <henochounsa@gmail.com>
2020-07-30 09:30:00 +0100

    add: metricsAggregator — aggregate 24h windows from DB

---
+++ src/core/metricsAggregator.ts
@@ -0,0 +1,26 @@
+import { getRecentMetrics } from '../db/repositories/metricsRepo';
+import { ProviderStats } from './scoringEngine';
+
+const SCORING_WINDOW_HOURS = 24;
+
+export async function aggregateProviderStats(
+  providerName: string,
+  operator: string,
+  country: string,
+): Promise<ProviderStats> {
+  const windows = await getRecentMetrics(providerName, operator, country, SCORING_WINDOW_HOURS);
+
+  if (windows.length === 0) {
+    return { successCount: 0, failureCount: 0, totalLatencyMs: 0, sampleCount: 0 };
+  }
+
+  return windows.reduce<ProviderStats>(
+    (acc, w) => ({
+      successCount:   acc.successCount   + w.successCount,
+      failureCount:   acc.failureCount   + w.failureCount,
+      totalLatencyMs: acc.totalLatencyMs + Number(w.totalLatencyMs),
+      sampleCount:    acc.sampleCount    + w.sampleCount,
+    }),
+    { successCount: 0, failureCount: 0, totalLatencyMs: 0, sampleCount: 0 },
+  );
+}
