692985f692ae964065d3b7998316070467e3e1fd
Hénoc HOUNSA <henochounsa@gmail.com>
2020-09-14 09:00:00 +0100

    chore: update core/index.ts with scoring and routing exports

--- src/core/index.ts
+++ src/core/index.ts
@@ -12,3 +12,17 @@ export {
   getAllActiveProviders,
 } from './providerRegistry';
 export type { ProviderConfig } from './providerRegistry';
+
+export {
+  computeProviderScore,
+  computeSuccessRate,
+  computeLatencyScore,
+  DEFAULT_WEIGHTS,
+} from './scoringEngine';
+export type { ProviderStats, ScoringWeights } from './scoringEngine';
+
+export { rankProviders, selectBestProvider } from './routingEngine';
+export type { ScoredProvider } from './routingEngine';
+
+export { buildFallbackChain, shouldFallback } from './fallbackChain';
+export { recordAttemptOutcome } from './metricsUpdater';
