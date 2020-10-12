50f1a659932c1054e9e2d909483590b7c2161e7b
Hénoc HOUNSA <henochounsa@gmail.com>
2020-10-12 09:30:00 +0100

    fix: graceful BullMQ worker drain on SIGTERM/SIGINT

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,29 @@
 import { connectWithRetry, disconnectPrisma } from './db/prismaClient';
 import { getRedisClient, closeRedis } from './cache/redis';
 import { buildServer } from './server';
+import { startRoutingWorker, stopRoutingWorker } from './jobs/routingWorker';
+import { closeQueue } from './jobs/routingQueue';
 import logger from './config/logger';
 import { env } from './config/env';
 
 async function bootstrap() {
   logger.info('Payment Routing Engine — starting up...');
-
   await connectWithRetry();
   getRedisClient();
-
   const server = await buildServer();
   await server.listen(env.PORT, '0.0.0.0');
   logger.info({ port: env.PORT }, 'HTTP server listening');
-
-  // TODO: register BullMQ workers
+  startRoutingWorker();
+  logger.info('All systems nominal');
 }
 
 async function gracefulShutdown(signal: string) {
-  logger.info({ signal }, 'Shutdown signal received');
+  logger.info({ signal }, 'Shutdown signal received — draining...');
+  await stopRoutingWorker();
+  await closeQueue();
   await disconnectPrisma();
   await closeRedis();
+  logger.info('Graceful shutdown complete');
   process.exit(0);
 }
 
