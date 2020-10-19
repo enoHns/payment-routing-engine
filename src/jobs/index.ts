939c2ad216cb1ebb76dd3ccaa166ff9f06a0b5a2
Hénoc HOUNSA <henochounsa@gmail.com>
2020-10-19 10:00:00 +0100

    add: jobs barrel export

---
+++ src/jobs/index.ts
@@ -0,0 +1,3 @@
+export { getRoutingQueue, enqueueRoutingJob, closeQueue } from './routingQueue';
+export type { RoutingJobPayload } from './routingQueue';
+export { startRoutingWorker, stopRoutingWorker } from './routingWorker';
