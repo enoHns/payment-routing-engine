054f52fbfb3413824cdc3de7f9746516367f271a
Hénoc HOUNSA <henochounsa@gmail.com>
2020-11-23 09:00:00 +0100

    add: payment request and response DTOs

---
+++ src/types/payment.ts
@@ -0,0 +1,33 @@
+export interface InitiatePaymentBody {
+  phone:          string;
+  amount:         number;
+  currency:       string;
+  idempotencyKey?: string;
+  webhookUrl?:    string;
+}
+
+export interface InitiatePaymentResponse {
+  transactionId: string;
+  status:        'INITIATED';
+}
+
+export interface TransactionResponse {
+  id:             string;
+  phone:          string;
+  country:        string;
+  operator:       string;
+  amount:         number;
+  currency:       string;
+  status:         string;
+  createdAt:      string;
+  updatedAt:      string;
+  attempts?:      AttemptResponse[];
+}
+
+export interface AttemptResponse {
+  id:           string;
+  providerName: string;
+  status:       string;
+  latencyMs?:   number | null;
+  createdAt:    string;
+}
