40554ac2d01973036c142a00c0b688964aa98e92
Hénoc HOUNSA <henochounsa@gmail.com>
2020-12-01 09:00:00 +0100

    refactor: register payment, transactions and webhook routes

--- src/server.ts
+++ src/server.ts
@@ -4,29 +4,45 @@ import rateLimit from 'fastify-rate-limit';
 import { getRedisClient } from './cache/redis';
 import { errorHandlerPlugin } from './plugins/errorHandler';
 import { healthRoutes } from './routes/health';
-import logger from './config/logger';
+import { paymentRoutes } from './routes/payment';
+import { transactionRoutes } from './routes/transactions';
+import { webhookRoutes } from './routes/webhook';
 import { env } from './config/env';
+import logger from './config/logger';
 
 export async function buildServer(): Promise<FastifyInstance> {
   const server = fastify({ logger: false, trustProxy: true });
 
   await server.register(helmet);
   await server.register(rateLimit, {
-    max:          env.RATE_LIMIT_MAX,
-    timeWindow:   env.RATE_LIMIT_WINDOW_MS,
-    redis:        getRedisClient(),
-    keyGenerator: (req) => (req.headers['x-forwarded-for'] as string) || req.ip,
+    max:         env.RATE_LIMIT_MAX,
+    timeWindow:  env.RATE_LIMIT_WINDOW_MS,
+    redis:       getRedisClient(),
+    keyGenerator: (req) => req.ip,
+  });
+
+  server.addHook('onRequest', (request, _reply, done) => {
+    request.requestId = request.id;
+    logger.info({ method: request.method, url: request.url, requestId: request.id }, 'Incoming request');
+    done();
+  });
+
+  server.addHook('onResponse', (request, reply, done) => {
+    logger.info({
+      method:      request.method,
+      url:         request.url,
+      statusCode:  reply.statusCode,
+      responseTime: reply.getResponseTime(),
+      requestId:   request.id,
+    }, 'Response sent');
+    done();
   });
 
   await server.register(errorHandlerPlugin);
   await server.register(healthRoutes);
-
-  server.addHook('onRequest', async (req) => {
-    logger.info({ method: req.method, url: req.url, reqId: req.id }, '→');
-  });
-  server.addHook('onResponse', async (req, reply) => {
-    logger.info({ method: req.method, url: req.url, status: reply.statusCode, reqId: req.id }, '←');
-  });
+  await server.register(paymentRoutes);
+  await server.register(transactionRoutes);
+  await server.register(webhookRoutes);
 
   return server;
 }
