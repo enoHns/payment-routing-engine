10217fa00c68fda9381fc6314c46b08f1215f363
Hénoc HOUNSA <henochounsa@gmail.com>
2020-12-14 10:00:00 +0100

    test: POST /webhook — success, error resilience, signature passthrough

---
+++ tests/unit/routes/webhook.test.ts
@@ -0,0 +1,57 @@
+jest.mock('../../../src/handlers/webhookHandler', () => ({
+  processWebhook: jest.fn(),
+}));
+
+import Fastify from 'fastify';
+import { webhookRoutes } from '../../../src/routes/webhook';
+import { processWebhook } from '../../../src/handlers/webhookHandler';
+
+const mockProcess = processWebhook as jest.MockedFunction<typeof processWebhook>;
+
+const buildApp = async () => {
+  const app = Fastify();
+  await app.register(webhookRoutes);
+  return app;
+};
+
+describe('POST /webhook/:provider', () => {
+  let app: ReturnType<typeof Fastify>;
+  beforeAll(async () => { app = await buildApp(); });
+  afterAll(async () => { await app.close(); });
+  beforeEach(() => jest.clearAllMocks());
+
+  it('returns 200 on SUCCESS webhook', async () => {
+    mockProcess.mockResolvedValue({ transactionId: 'tx-1', status: 'SUCCESS' });
+    const res = await app.inject({
+      method:  'POST',
+      url:     '/webhook/kkiapay',
+      payload: { transactionId: 'ptx-1', status: 'SUCCESS' },
+      headers: { 'content-type': 'application/json' },
+    });
+    expect(res.statusCode).toBe(200);
+    expect(JSON.parse(res.body).status).toBe('SUCCESS');
+  });
+
+  it('returns 200 even on processWebhook error (idempotent webhook response)', async () => {
+    mockProcess.mockRejectedValue(new Error('DB error'));
+    const res = await app.inject({
+      method:  'POST',
+      url:     '/webhook/cinetpay',
+      payload: { cpm_trans_id: 'ptx-2', cpm_result: '00' },
+      headers: { 'content-type': 'application/json' },
+    });
+    expect(res.statusCode).toBe(200);
+    expect(JSON.parse(res.body).status).toBe('ERROR');
+  });
+
+  it('passes signature header to processWebhook', async () => {
+    mockProcess.mockResolvedValue({ transactionId: '', status: 'IGNORED' });
+    await app.inject({
+      method:  'POST',
+      url:     '/webhook/kkiapay',
+      payload: { transactionId: 'ptx-1', status: 'SUCCESS' },
+      headers: { 'content-type': 'application/json', 'x-kkiapay-signature': 'sha256=abc' },
+    });
+    expect(mockProcess).toHaveBeenCalledWith('kkiapay', expect.any(Object), 'sha256=abc');
+  });
+});
