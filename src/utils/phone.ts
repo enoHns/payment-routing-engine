12f169708807a56cfd4074bd5afdaffd2c6e98f1
Hénoc HOUNSA <henochounsa@gmail.com>
2021-07-05 09:00:00 +0100

    fix: phone normalizer — handle 00xx prefix, strip separators

--- src/utils/phone.ts
+++ src/utils/phone.ts
@@ -1,28 +1,30 @@
-const CALLING_CODES: Record<string, string> = {
-  BJ: '229',
-  CI: '225',
-  SN: '221',
-  TG: '228',
-  NE: '227',
-  GN: '224',
-  ML: '223',
-  BF: '226',
-  CM: '237',
-  SL: '232',
+export const CALLING_CODES: Record<string, string> = {
+  BJ: '229', CI: '225', SN: '221', TG: '228',
+  NE: '227', BF: '226', ML: '223', GN: '224',
+  CM: '237', GH: '233',
 };
 
+const COUNTRY_FROM_CODE: Record<string, string> = Object.fromEntries(
+  Object.entries(CALLING_CODES).map(([c, code]) => [code, c]),
+);
+
 /**
- * Normalize a phone number to local format (no country code, no separators).
- * E.g. "+22996123456" (BJ) → "96123456"
+ * Normalize to E.164 format (+<calling_code><number>).
+ * Already in E.164: returned as-is.
+ * Starts with 00: 00 → +
+ * Otherwise: assumed local (no prefix); kept unchanged (caller should add calling code).
  */
-export function normalizePhone(phone: string, country: string): string {
-  let p = phone.replace(/[\s\-\.]/g, '');
-  if (p.startsWith('+')) p = p.slice(1);
-  const cc = CALLING_CODES[country];
-  if (cc && p.startsWith(cc)) p = p.slice(cc.length);
-  return p;
+export function normalizePhone(phone: string): string {
+  const stripped = phone.replace(/[\s\-().]/g, '');
+  if (stripped.startsWith('+')) return stripped;
+  if (stripped.startsWith('00')) return '+' + stripped.slice(2);
+  return stripped;
 }
 
-export function getCallingCode(country: string): string | undefined {
-  return CALLING_CODES[country];
+export function getCallingCode(countryCode: string): string | undefined {
+  return CALLING_CODES[countryCode.toUpperCase()];
+}
+
+export function getCountryFromCallingCode(code: string): string | undefined {
+  return COUNTRY_FROM_CODE[code];
 }
