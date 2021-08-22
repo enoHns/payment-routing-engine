63c9fd681b3f49ee22079a921feeb1b93ada50ff
Hénoc HOUNSA <henochounsa@gmail.com>
2021-08-22 09:45:00 +0200

    fix: kkiapay config — use env.KKIAPAY_BASE_URL to allow sandbox/production switching (was hardcoded to production)

--- src/providers/kkiapay/kkiapay.config.ts
+++ src/providers/kkiapay/kkiapay.config.ts
@@ -4,6 +4,6 @@ export const kkiapayConfig = {
   privateKey: env.KKIAPAY_PRIVATE_KEY,
   publicKey:  env.KKIAPAY_PUBLIC_KEY,
   hmacKey:    env.KKIAPAY_HMAC_KEY,
-  baseUrl:    'https://api.kkiapay.me',
-  timeout:    10000,
+  baseUrl:    env.KKIAPAY_BASE_URL,   // sandbox: https://sandbox-api.kkiapay.me
+  timeout:    env.KKIAPAY_TIMEOUT_MS,
 };
