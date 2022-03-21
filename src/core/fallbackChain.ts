74720af96b2fbad07cd350a8905e2f5617509c77
Hénoc HOUNSA <henochounsa@gmail.com>
2022-03-21 10:00:00 +0100

    fix: fallbackChain — filter excludeProviders before slicing chain

--- src/core/fallbackChain.ts
+++ src/core/fallbackChain.ts
@@ -1,48 +1,37 @@
-import { ScoredProvider, rankProviders } from './routingEngine';
+import { rankProviders } from './routingEngine';
 import { isRetryable } from '../providers/base/ProviderResult';
 import logger from '../config/logger';
+import type { ScoredProvider } from './routingEngine';
 
-export interface FallbackOptions {
-  maxAttempts:     number;
+interface FallbackOptions {
+  maxAttempts:       number;
   excludeProviders?: string[];
 }
 
-const DEFAULT_OPTIONS: FallbackOptions = { maxAttempts: 3 };
-
-/**
- * Returns the ordered list of providers to try, excluding those already
- * attempted and respecting maxAttempts.
- */
 export async function buildFallbackChain(
   operator: string,
-  country: string,
-  options: FallbackOptions = DEFAULT_OPTIONS,
+  country:  string,
+  options:  FallbackOptions,
 ): Promise<ScoredProvider[]> {
-  const exclude = new Set(options.excludeProviders ?? []);
-  const ranked  = await rankProviders(operator, country);
-  const chain   = ranked.filter(sp => !exclude.has(sp.provider.name));
-
-  return chain.slice(0, options.maxAttempts);
+  const { maxAttempts, excludeProviders = [] } = options;
+  const ranked = await rankProviders(operator, country);
+  return ranked
+    .filter(sp => !excludeProviders.includes(sp.provider.name))
+    .slice(0, maxAttempts);
 }
 
-/**
- * Determines whether an error justifies trying the next provider
- * in the chain, or should abort the whole transaction.
- */
 export function shouldFallback(errorCode: string): boolean {
-  // Retryable errors (network, timeout) → try next provider
-  // Business errors (insufficient funds, duplicate) → abort
   return isRetryable(errorCode);
 }
 
 export function logFallback(
   transactionId: string,
-  failedProvider: string,
-  nextProvider: string,
-  errorCode: string,
+  fromProvider:  string,
+  toProvider:    string,
+  reason:        string,
 ): void {
-  logger.warn(
-    { transactionId, failedProvider, nextProvider, errorCode },
-    'Provider failed — falling back',
+  logger.info(
+    { transactionId, fromProvider, toProvider, reason },
+    `Falling back from ${fromProvider} to ${toProvider}`,
   );
 }
