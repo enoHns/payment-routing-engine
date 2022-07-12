abf3eff9b57059e5158639fa2a98792d1b82f125
Hénoc HOUNSA <henochounsa@gmail.com>
2022-07-12 09:00:00 +0100

    chore: vitest.config.ts — migrate from jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,26 +1,2 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  roots: ['<rootDir>/tests'],
-  testMatch: ['**/*.test.ts'],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-  },
-  globals: {
-    'ts-jest': {
-      tsConfig: 'tsconfig.json',
-    },
-  },
-  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
-  coverageDirectory: 'coverage',
-  collectCoverageFrom: [
-    'src/**/*.ts',
-    '!src/index.ts',
-  ],
-  coverageThreshold: {
-    global: {
-      lines: 70,
-      branches: 65,
-    },
-  },
-};
+// Replaced by vitest.config.ts — kept for reference only
+module.exports = {};
