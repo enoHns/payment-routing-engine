46fb7c3162349456867d9ff9f002c7b75e4c1f43
Hénoc HOUNSA <henochounsa@gmail.com>
2023-11-20 14:47:33 +0100

    chore: vitest — add coverage thresholds (70%) and wire up setup file

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,10 +5,17 @@ export default defineConfig({
     globals:     true,
     environment: 'node',
     include:     ['tests/**/*.test.ts'],
+    setupFiles:  ['./tests/setup.ts'],
     coverage: {
-      provider: 'v8',
-      reporter: ['text', 'lcov', 'html'],
-      exclude:  ['**/node_modules/**', '**/dist/**'],
+      provider:  'v8',
+      reporter:  ['text', 'lcov', 'html'],
+      exclude:   ['**/node_modules/**', '**/dist/**', '**/tests/**'],
+      thresholds: {
+        lines:      70,
+        functions:  70,
+        branches:   60,
+        statements: 70,
+      },
     },
   },
 });
